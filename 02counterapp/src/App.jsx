ca0e9b1741c75f53bc4aae6225369524ec1eceaf
Sahil Thakur <mesahilthakur@gmail.com>
2023-09-01 19:10:30 +0530

    counter app added

---
+++ 02counterapp/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(0);
+
+  const increaseCounter = ()=>{
+    if(counter<20){
+      setCounter(counter+1);
+    }
+  }
+
+  const decreaseCounter = ()=>{
+    if(counter>0){
+      setCounter(counter-1);
+    }
+  }
+
+  return (
+    <>
+    <h1>Welcome to Counter App</h1>
+    <h2>Here you can increase or decrease counter between 0 to 20</h2>
+    <div>Counter Value : <span className='counter-color'>{counter}</span></div>
+    <br />
+    <button onClick={increaseCounter}>Increase</button>
+    <button onClick={()=>decreaseCounter()}>Decrease</button> 
+    </>
+  )
+}
+
+export default App
