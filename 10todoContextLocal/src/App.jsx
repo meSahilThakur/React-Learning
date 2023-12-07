d16af76e0b9efa95880f14b1d2ce19165cd4090f
Sahil Thakur <mesahilthakur@gmail.com>
2023-12-07 17:47:50 +0530

    todoForm added

--- 10todoContextLocal/src/App.jsx
+++ 10todoContextLocal/src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import {TodoProvider, useTodo} from './contexts'
+import { useEffect, useState } from "react";
+import {TodoProvider} from './contexts'
 import "./App.css";
 
 function App() {
@@ -21,6 +21,21 @@ function App() {
   const toggleCompleted = (id) => {
     setTodos((prev)=> prev.map((prevTodo)=>prevTodo.id === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo))
   }
+
+  //local Storage
+  useEffect(()=>{
+    const todos = JSON.parse(localStorage.getItem("todos"));
+
+    if(todos && todos.length > 0){
+      setTodos(todos);
+    }
+  },[])
+
+  useEffect(()=>{
+    localStorage.setItem("todos", JSON.stringify("todos"));
+  },[todos])
+
+
 
   return (
     <TodoProvider value={{ todos, addTodo, updateTodo, deleteTodo, toggleCompleted}}>
